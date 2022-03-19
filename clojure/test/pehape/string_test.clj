8bcb764433459b2fa289d7d907a594499c8a4ad5
elianiva <dicha.arkana03@gmail.com>
2022-03-19 12:18:37 +0700

    test(clojure): better coverage

--- clojure/test/pehape/string_test.clj
+++ clojure/test/pehape/string_test.clj
@@ -21,10 +21,16 @@
     #_{:clj-kondo/ignore [:inline-def]}
     (def content "Hello pehape world")
     (is (= (explode "  " content) ["Hello pehape world"])))
-  (testing "Explode Space Separated String By Empty String Throws Exception"
+  (testing "Explode Space Separated String By Empty String Or Nil Throws Exception"
     #_{:clj-kondo/ignore [:inline-def]}
     (def content "Hello pehape world")
-    (is (thrown-with-msg? IllegalArgumentException #"separator cannot be nil or empty" (explode "" content)))))
+    (is (thrown-with-msg? IllegalArgumentException #"separator cannot be nil or empty" (explode "" content)))
+    (is (thrown-with-msg? IllegalArgumentException #"separator cannot be nil or empty" (explode nil content)))
+    (is (thrown-with-msg? IllegalArgumentException #"separator cannot be nil or empty" (explode "" content 1)))
+    (is (thrown-with-msg? IllegalArgumentException #"separator cannot be nil or empty" (explode nil content 1))))
+  (testing "Explode Space Separated Nil Content Throws Exception"
+    (is (thrown-with-msg? IllegalArgumentException #"content cannot be nil" (explode " " nil)))
+    (is (thrown-with-msg? IllegalArgumentException #"content cannot be nil" (explode " " nil 1)))))
 
 
 (deftest implode-test
