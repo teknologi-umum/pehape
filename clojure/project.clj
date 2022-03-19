61965cc7b58f75dcc8608c52211d34ba30d3f869
elianiva <dicha.arkana03@gmail.com>
2022-03-19 11:52:21 +0700

    chore(clojure): add CI workflow

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,6 +1,7 @@
 (defproject com.teknologiumum/pehape-clj "0.0.1"
   :description "PHP stdlib global functions implemented in Clojure"
   :url "https://github.com/teknologi-umum/pehape"
+  :plugins [[lein-cloverage "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :profiles {:dev {:dependencies [[cider/cider-nrepl "0.28.3"]]}}
   :target-path "target/%s"
