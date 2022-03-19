ac29e2fb8689f9303452b0df9d30ebcee613f07c
afman42 <afifarman50@gmail.com>
2022-03-19 17:31:09 +0700

    Add Simplecov for coverage

--- ruby/spec/spec_helper.rb
+++ ruby/spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "../lib/pehape/pehape"
+require "simplecov"
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
