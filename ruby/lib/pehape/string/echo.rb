8b0aedc51c0f9b20b784eafc357f60d10688d6ae
afman42 <afifarman50@gmail.com>
2022-03-20 10:06:19 +0700

    Add Echo to Ruby

---
+++ ruby/lib/pehape/string/echo.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Pehape
+  def self.echo(string)
+    puts string
+  end
+end
---
+++ ruby/spec/pehape/string/echo_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Pehape do
+  it "echo all type data" do
+    expect(Pehape.echo("Pe ha pe")).to equal(nil)
+    expect(Pehape.echo(nil)).to equal(nil)
+    expect(Pehape.echo(true)).to equal(nil)
+    expect(Pehape.echo(1)).to equal(nil)
+    expect(Pehape.echo(-3)).to equal(nil)
+    expect(Pehape.echo(["Pe","Ha"])).to equal(nil)
+    expect(Pehape.echo({ :Pe => "Ha"})).to equal(nil)
+  end
+end
