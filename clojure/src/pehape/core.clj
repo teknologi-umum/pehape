690bffc3bc123662bc4814da67855a4d60e08bec
elianiva <dicha.arkana03@gmail.com>
2022-03-28 11:13:52 +0700

    refactor(clojure): restructure project namespace

--- clojure/src/pehape/core.clj
+++ clojure/src/pehape/core.clj
@@ -1,1 +1,9 @@
-(ns pehape.core)
+(ns pehape.core
+  (:require [pehape.string.explode :as str-explode]
+            [pehape.string.implode :as str-implode]
+            [pehape.string.levenshtein :as str-levenshtein]))
+
+
+(def explode str-explode/explode)
+(def implode str-implode/implode)
+(def levenshtein str-levenshtein/levenshtein)
--- clojure/src/pehape/string/levenshtein.clj
+++ clojure/src/pehape/string/levenshtein.clj
@@ -1,48 +1,4 @@
-(ns pehape.string
-  (:require [clojure.string :as str]))
-
-
-(defn explode
-  "The Explode function breaks a string into an array.
-
-   Arguments:
-   - `separator` Specifies where to break the string.
-   - `content` The string to split.
-   - `limit` Specifies the number of array elements to return.
-   If limit is greater than 0, then an array with a maximum of limit
-   elements is returned. If limit is less than 0 then an array excluding
-   the last -limit elements is returned. If limit is 0 then an array with
-   one element is returned.
-   Returns:
-   - An array of strings."
-  {:added "0.0.1"}
-  ([separator content]
-   (cond
-     (or (nil? separator)
-         (= separator "")) (throw (IllegalArgumentException. "separator cannot be nil or empty"))
-     (nil? content) (throw (IllegalArgumentException. "content cannot be nil"))
-     :else (str/split content (re-pattern separator))))
-  ([separator content limit]
-   (cond
-     (or (nil? separator)
-         (= separator "")) (throw (IllegalArgumentException. "separator cannot be nil or empty"))
-     (nil? content) (throw (IllegalArgumentException. "content cannot be nil"))
-     (> limit 0) (take limit (str/split content (re-pattern separator)))
-     (< limit 0) (drop-last (- limit) (str/split content (re-pattern separator)))
-     :else (take 1 (str/split content (re-pattern separator))))))
-
-
-(defn implode
-  "The Implode function joins array elements with a string.
-
-   Arguments:
-   - `separator` Specifies what to put between the array elements.
-   - `array` The array to join to a string.
-   Returns:
-   - A string from elements of an array"
-  {:added "0.0.1"}
-  [separator array]
-  (str/join separator array))
+(ns pehape.string.levenshtein)
 
 
 (defn levenshtein
@@ -84,7 +40,8 @@
            (let [c0 (atom (+ (if @swapped
                                (nth @p2 i2)
                                (nth @p1 i2))
-                             (if (= (nth str1 i1) (nth str2 i2))
+                             (if (= (nth str1 i1)
+                                    (nth str2 i2))
                                0
                                replace-cost)))
                  c1 (atom (+ delete-cost
