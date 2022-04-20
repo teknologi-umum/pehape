c66169cc1932fa3129a31fa75e6d048a61eff3ed
Krishna Rowter <krishna.rowter@gmail.com>
2022-04-20 17:20:41 +0700

    chore: fix eslint warnings

--- typescript/src/string/implode.ts
+++ typescript/src/string/implode.ts
@@ -1,5 +1,5 @@
 export const implode = (separator: string | null, arrayOfString: string[]): string => {
-    if (separator === null) separator = ''
+    if (separator === null) separator = "";
     
-    return arrayOfString.join(separator)
-}
+    return arrayOfString.join(separator);
+};
