88ad1459e42b5473fdf9ea1eaa2706c7cd1b1c5d
fadilaaaa <57476855+fadilaaaa@users.noreply.github.com>
2022-10-02 18:22:46 +0700

    Update add_slashes_test.go

--- go/add_slashes_test.go
+++ go/add_slashes_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestAddSlashes(t *testing.T) {
 	t.Run("double quote", func(t *testing.T) {
-		if res := PHP.Addslashes(`nano"nan"ya`); res != `nano\"nan\"ya` {
+		if res := PHP.AddSlashes(`nano"nan"ya`); res != `nano\"nan\"ya` {
 			t.Errorf(`expected nano\"nan\"ya, got %s`, res)
 		}
 	})
 	t.Run("slash", func(t *testing.T) {
-		if res := PHP.Addslashes(`nano\nan\ya`); res != `nano\\nan\\ya` {
+		if res := PHP.AddSlashes(`nano\nan\ya`); res != `nano\\nan\\ya` {
 			t.Errorf(`expected nano\\nan\\ya, got %s`, res)
 		}
 	})
