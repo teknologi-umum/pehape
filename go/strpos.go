12861922e3b52ef0031f86ccf8ab1014ee2e16ee
yogipristiawan <yogistanada252@gmail.com>
2022-10-03 18:59:46 +0700

    refactor: variable name

--- go/strpos.go
+++ go/strpos.go
@@ -16,16 +16,16 @@ var (
 // - int => Returns the position of the first occurrence of a string
 // - error => errors if the given offsed is invalid or if string not found
 func Strpos(str, find string, offset ...int) (int, error) {
-	var o int = 0
+	var defaultOffset int = 0
 	if len(offset) > 0 {
-		o = offset[0]
-		if o >= len(str) || len(str)+o < 0 {
+		defaultOffset = offset[0]
+		if defaultOffset >= len(str) || len(str)+defaultOffset < 0 {
 			return 0, ErrStrposInvalidOffset
 		}
 	}
 
-	if o < 0 {
-		for i := len(str) + o; i <= (len(str) - len(find)); i++ {
+	if defaultOffset < 0 {
+		for i := len(str) + defaultOffset; i <= (len(str) - len(find)); i++ {
 			if str[i:i+len(find)] == find {
 				return i, nil
 			}
@@ -33,7 +33,7 @@ func Strpos(str, find string, offset ...int) (int, error) {
 		return 0, ErrStrposStringNotFound
 	}
 
-	for i := o; i <= (len(str) - len(find)); i++ {
+	for i := defaultOffset; i <= (len(str) - len(find)); i++ {
 		if str[i:i+len(find)] == find {
 			return i, nil
 		}
