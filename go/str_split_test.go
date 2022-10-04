00b20d4dca2da8ffcd010eea83a23501f2d49971
yogipristiawan <yogistanada252@gmail.com>
2022-10-04 09:25:04 +0700

    refactor: change variable name

--- go/str_split_test.go
+++ go/str_split_test.go
@@ -18,14 +18,14 @@ func TestStrSplit(t *testing.T) {
 	}
 
 	t.Run("It should return error if the given length is less than 1", func(t *testing.T) {
-		tt := &test{
+		test := &test{
 			param: param{
 				str:    "Hello",
 				length: []int{0},
 			},
 		}
 
-		_, err := PHP.StrSplit(tt.param.str, tt.param.length...)
+		_, err := PHP.StrSplit(test.param.str, test.param.length...)
 		if err == nil {
 			t.Errorf("expect error, but got error nil")
 		}
@@ -35,34 +35,34 @@ func TestStrSplit(t *testing.T) {
 	})
 
 	t.Run("It should split the given string correctly if the given length is empty", func(t *testing.T) {
-		tt := &test{
+		test := &test{
 			param: param{
 				str: "Hello",
 			},
 			expect: []string{"H", "e", "l", "l", "o"},
 		}
 
-		res, err := PHP.StrSplit(tt.param.str)
+		res, err := PHP.StrSplit(test.param.str)
 
 		if err != nil {
 			t.Errorf("expect error nil, but got %s", err)
 		}
 		if len(res[0]) != 1 {
-			t.Errorf("expect %v, but got %v", tt.expect, res)
+			t.Errorf("expect %v, but got %v", test.expect, res)
 		}
-		if len(res) != len(tt.expect) {
-			t.Errorf("expect %v, but got %v", tt.expect, res)
+		if len(res) != len(test.expect) {
+			t.Errorf("expect %v, but got %v", test.expect, res)
 		}
 		for i, v := range res {
-			if tt.expect[i] != v {
-				t.Errorf("expect %v, but got %v", tt.expect, res)
+			if test.expect[i] != v {
+				t.Errorf("expect %v, but got %v", test.expect, res)
 				return
 			}
 		}
 	})
 
 	t.Run("It should split the given string correctly if the given length is not empty", func(t *testing.T) {
-		tt := &test{
+		test := &test{
 			param: param{
 				str:    "Hello",
 				length: []int{2},
@@ -70,20 +70,20 @@ func TestStrSplit(t *testing.T) {
 			expect: []string{"He", "ll", "o"},
 		}
 
-		res, err := PHP.StrSplit(tt.param.str, tt.param.length...)
+		res, err := PHP.StrSplit(test.param.str, test.param.length...)
 
 		if err != nil {
 			t.Errorf("expect error nil, but got %s", err)
 		}
-		if len(res[0]) != tt.param.length[0] {
-			t.Errorf("expect %v, but got %v", tt.expect, res)
+		if len(res[0]) != test.param.length[0] {
+			t.Errorf("expect %v, but got %v", test.expect, res)
 		}
-		if len(res) != len(tt.expect) {
-			t.Errorf("expect %v, but got %v", tt.expect, res)
+		if len(res) != len(test.expect) {
+			t.Errorf("expect %v, but got %v", test.expect, res)
 		}
 		for i, v := range res {
-			if tt.expect[i] != v {
-				t.Errorf("expect %v, but got %v", tt.expect, res)
+			if test.expect[i] != v {
+				t.Errorf("expect %v, but got %v", test.expect, res)
 				return
 			}
 		}
