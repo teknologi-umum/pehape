0965918a8c2dbfb13f45a53f0117222569593d94
dkelxldk <aditiaaldama221@gmail.com>
2022-10-06 14:41:46 +0700

    docs(typescript): add jsdocs to addslashes

--- typescript/src/string/addslashes.ts
+++ typescript/src/string/addslashes.ts
@@ -1,1 +1,6 @@
+/**
+ * Replacing the single quote, double quote with backslash
+ * @param {string} text
+ * @returns {string}
+ */
 export const addslashes = (text: string): string => text.replace(/'|"/g, "\\'");
