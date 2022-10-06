8c8df3b978962750ea35679af9e95d7c002514f0
afman42 <afifarman50@gmail.com>
2022-10-06 18:47:33 +0700

    change docstring

--- typescript/src/string/str_repeat.ts
+++ typescript/src/string/str_repeat.ts
@@ -1,8 +1,8 @@
 /**
  * str_repeat — Repeat a string
- * @param {string} str
- * @param {number} times
- * @returns {string} Returns string repeated times times.
+ * @param str - The string to be repeated.
+ * @param times - Number of time the `str` string should be repeated. `times` has to be greater than or equal to 0. If the `times` is set to 0, the function will return an empty string.
+ * @returns Returns string repeated times.
  */
 export const str_repeat = (str: string, times: number): string => {
   if (typeof times !== "number") throw new Error("Argument must be a number");
