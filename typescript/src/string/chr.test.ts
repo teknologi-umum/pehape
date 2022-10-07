9125c437696b4669326167e433477001f634ccc5
afman42 <afifarman50@gmail.com>
2022-10-07 13:49:34 +0700

    missed spell character

--- typescript/src/string/chr.test.ts
+++ typescript/src/string/chr.test.ts
@@ -2,14 +2,14 @@ import { chr } from "./chr";
 
 describe("chr", () => {
   it("ascii decimal value chr", () => {
-    expect(Pehape.chr(52)).toEqual("4");
+    expect(chr(52)).toEqual("4");
   });
 
   it("ascii octal value chr", () => {
-    expect(Pehape.chr(0o52)).toEqual("*");
+    expect(chr(0o52)).toEqual("*");
   });
 
   it("ascii hex value chr", () => {
-    expect(Pehape.chr(0x52)).toEqual("R");
+    expect(chr(0x52)).toEqual("R");
   });
 });
