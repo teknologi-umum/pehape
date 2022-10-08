295b08812b93349f40d53bd6a9b7d16c3d644548
dkelxldk <aditiaaldama221@gmail.com>
2022-10-08 12:43:35 +0700

    docs(typescript-lcfirst): add JSDoc

--- typescript/src/string/lcfirst.ts
+++ typescript/src/string/lcfirst.ts
@@ -1,1 +1,6 @@
+/**
+ * Make a string's first character lowercase.
+ * @param {String} text The input text.
+ * @returns {String} a string with the first character of string lowercased.
+ */
 export const lcfirst = (text: string): string => text.charAt(0).toLowerCase() + text.slice(1);
