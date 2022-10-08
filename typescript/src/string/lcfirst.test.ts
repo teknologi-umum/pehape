6cdf8f30960242c41d2cfd0adcd4937bce29cc1e
dkelxldk <aditiaaldama221@gmail.com>
2022-10-08 12:43:58 +0700

    test(typescript-lcfirst): add more test case

--- typescript/src/string/lcfirst.test.ts
+++ typescript/src/string/lcfirst.test.ts
@@ -2,7 +2,19 @@ import { lcfirst } from "./lcfirst";
 
 const text = "Shizumu you ni tokete yuku you ni.";
 
-describe("valid parameters", () => {
+describe("parameter is empty string", () => {
+  it("should return empty string", () => {
+    expect(lcfirst("")).toBe("");
+  });
+});
+
+describe("parameter is whitespace", () => {
+  it("should return the whitespace", () => {
+    expect(lcfirst("  ")).toBe("  ");
+  });
+});
+
+describe("parameter not empty string", () => {
   it("should return lowercased first characters from capital text", () => {
     expect(lcfirst(text)).toEqual("shizumu you ni tokete yuku you ni.");
   });
@@ -13,5 +25,18 @@ describe("valid parameters", () => {
 
   it("should return lowercased first characters from all lowercased text", () => {
     expect(lcfirst(text.toLowerCase())).toEqual("shizumu you ni tokete yuku you ni.");
+  });
+
+  it("should return the same for non alphabetical characters", () => {
+    expect(lcfirst("沈むように溶けてゆくように")).toEqual("沈むように溶けてゆくように");
+  });
+
+  it("should return lowercased first characters from alphabetical with special characters", () => {
+    expect(lcfirst("Éxample")).toEqual("éxample");
+  });
+
+  it("should return the same as input", () => {
+    expect(lcfirst("123456789")).toBe("123456789");
+    expect(lcfirst("!@#$%%")).toBe("!@#$%%");
   });
 });
