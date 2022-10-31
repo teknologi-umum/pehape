cb225b6bcfbd8b53df61e0868f88d5f4acb4f5b9
DimensionalDragon <andhikangurah@gmail.com>
2022-10-31 22:38:40 +0800

    feat(TypeScript): ltrim

---
+++ typescript/src/string/ltrim.test.ts
@@ -0,0 +1,36 @@
+import { ltrim } from "./ltrim";
+
+describe("ltrim with default characters", () => {
+  it("should return trimmed string", () => {
+    expect(ltrim("    Hello, World!")).toStrictEqual("Hello, World!");
+    expect(ltrim("\n  \t Hello, World!")).toStrictEqual("Hello, World!");
+    expect(ltrim("\0 \vHello, World!")).toStrictEqual("Hello, World!");
+  });
+
+  it("should not trim the right side of the string", () => {
+    expect(ltrim("Hello, World!    ")).toStrictEqual("Hello, World!    ");
+    expect(ltrim("    Hello, World!    ")).toStrictEqual("Hello, World!    ");
+  });
+
+  it("should return the string if there is nothing to be trimmed", () => {
+    expect(ltrim("Hello, World!")).toStrictEqual("Hello, World!");
+  });
+});
+
+describe("ltrim with different characters", () => {
+  it("should return trimmed string with different characters", () => {
+    expect(ltrim("rrrrrrrHello, World!", "r")).toStrictEqual("Hello, World!");
+    expect(ltrim("rtrtrttrrHello, World!", "rt")).toStrictEqual("Hello, World!");
+    expect(ltrim("0293174856Hello, World!", "0123456789")).toStrictEqual("Hello, World!");
+  });
+
+  it("should not trim the right side of the string", () => {
+    expect(ltrim("Hello, World!rrrrrr", "r")).toStrictEqual("Hello, World!rrrrrr");
+    expect(ltrim("rrrrrHello, World!rrrrr", "r")).toStrictEqual("Hello, World!rrrrr");
+  });
+
+  it("should trim regex special characters", () => {
+    expect(ltrim(".*+Hello, World!", "*.+")).toStrictEqual("Hello, World!");
+    expect(ltrim("[$$$*]Hello, World!", "*$[]")).toStrictEqual("Hello, World!");
+  });
+});
---
+++ typescript/src/string/ltrim.ts
@@ -0,0 +1,18 @@
+/**
+ * ltrim — Strip whitespace (or other characters) from the beginning of a string.
+ * @param str - The input string.
+ * @param characters - You can also specify the characters you want to strip, by means of the characters parameter. Simply list all characters that you want to be stripped. With .. you can specify a range of characters.
+ * @returns string with whitespace stripped from the beginning of string
+ *   Without the second parameter, ltrim() will strip these characters:
+ *   " " (ASCII 32 (0x20)), an ordinary space.
+ *   "\t" (ASCII 9 (0x09)), a tab.
+ *   "\n" (ASCII 10 (0x0A)), a new line (line feed).
+ *   "\r" (ASCII 13 (0x0D)), a carriage return.
+ *   "\0" (ASCII 0 (0x00)), the NUL-byte.
+ *   "\v" (ASCII 11 (0x0B)), a vertical tab.
+*/
+export const ltrim = (str: string, characters = " \n\r\t\v\x00"): string => {
+  const charactersEscaped = characters.replace(/([\\^$.|?*+()[\]{}])/g, "\\$1");
+  const charactersRegex = new RegExp(`^[${charactersEscaped}]+`);
+  return str.replace(charactersRegex, "");
+};
