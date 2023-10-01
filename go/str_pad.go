7caf0c6fc0f249f03c6b5db723dd43ef74051f6e
Rahmat Fathoni <mr.rahmat.fathoni@gmail.com>
2023-10-01 19:07:33 +0800

    refactor(str_pad): add comment and rename variable

--- go/str_pad.go
+++ go/str_pad.go
@@ -20,6 +20,14 @@ var (
 	ErrPadMustNotEmptyString = errors.New("pad must be a non-empty string")
 )
 
+// StrPad Pad a string to a certain length with another string
+// This function returns the string string padded on the left, the right,
+// or both sides to the specified padding length.
+// If the optional argument pad_string is not supplied, the string is padded with spaces,
+// otherwise it is padded with characters from pad_string up to the limit.
+// Optional params:
+// - pad_string => string or int or float64
+// - pad_type => STR_PAD_LEFT, STR_PAD_RIGHT, STR_PAD_BOTH
 func StrPad(s string, length int, args ...any) (string, error) {
 	// first check args length
 	if len(args) > 2 {
@@ -31,19 +39,19 @@ func StrPad(s string, length int, args ...any) (string, error) {
 		return s, nil
 	}
 
-	pad := " "
+	padString := " "
 
 	// set pad
 	if len(args) > 0 {
 		switch args[0].(type) {
 		case string, float64, int:
-			pad = fmt.Sprintf("%v", args[0])
+			padString = fmt.Sprintf("%v", args[0])
 		default:
 			return "", ErrPadInvalid
 		}
 
 		// validate pad only when args[0] not empty
-		if pad == "" {
+		if padString == "" {
 			return "", ErrPadMustNotEmptyString
 		}
 	}
@@ -67,11 +75,11 @@ func StrPad(s string, length int, args ...any) (string, error) {
 	}
 
 	// calculate repeat count
-	leftRightRepeatCount := int(math.Ceil(float64(length-len(s)) / float64(len(pad))))
+	leftRightRepeatCount := int(math.Ceil(float64(length-len(s)) / float64(len(padString))))
 
 	switch padType {
 	case STR_PAD_LEFT:
-		return strings.Repeat(pad, leftRightRepeatCount)[:length-len(s)] + s, nil
+		return strings.Repeat(padString, leftRightRepeatCount)[:length-len(s)] + s, nil
 	case STR_PAD_BOTH:
 		lenRightFloat64 := float64(length-len(s)) / 2
 
@@ -79,8 +87,8 @@ func StrPad(s string, length int, args ...any) (string, error) {
 		lenLeft := length - len(s) - lenRight
 
 		// +1 to avoid index out of range
-		return strings.Repeat(pad, leftRightRepeatCount/2+1)[:lenLeft] + s + strings.Repeat(pad, leftRightRepeatCount/2+1)[:lenRight], nil
+		return strings.Repeat(padString, leftRightRepeatCount/2+1)[:lenLeft] + s + strings.Repeat(padString, leftRightRepeatCount/2+1)[:lenRight], nil
 	default:
-		return s + strings.Repeat(pad, leftRightRepeatCount)[:length-len(s)], nil
+		return s + strings.Repeat(padString, leftRightRepeatCount)[:length-len(s)], nil
 	}
 }
