78b2aa459fd97e481e9942c4486e727f63f6af58
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2024-12-07 12:24:02 +0700

    Clean up linter warnings

--- csharp/Pehape/String/Md5.cs
+++ csharp/Pehape/String/Md5.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using System.Text;
 
 namespace Pehape {
+	// ReSharper disable once InconsistentNaming
 	public static partial class PHP {
 		/// <summary>
 		/// The Md5Raw function calculates the md5 hash of a string
@@ -9,9 +10,10 @@ namespace Pehape {
 		/// <param name="input">String to be calculated</param>
 		/// <returns>16 character binary format</returns>
 		public static byte[] Md5Raw(string input) {
-			using var md5 = System.Security.Cryptography.MD5.Create();
 			var inputBytes = Encoding.ASCII.GetBytes(input);
-			var hashBytes = md5.ComputeHash(inputBytes);
+#pragma warning disable CA5351
+			var hashBytes = System.Security.Cryptography.MD5.HashData(inputBytes);
+#pragma warning restore CA5351
 			return hashBytes;
 		}
 
@@ -23,9 +25,7 @@ namespace Pehape {
 		public static string Md5(string input) {
 			var hashBytes = Md5Raw(input);
 			var sb = new StringBuilder();
-			foreach (var t in hashBytes) {
-				sb.Append(t.ToString("x2", CultureInfo.InvariantCulture));
-			}
+			foreach (var t in hashBytes) sb.Append(t.ToString("x2", CultureInfo.InvariantCulture));
 
 			return sb.ToString();
 		}
